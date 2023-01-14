0a46a8f611fceca16e08a60003b31f209904338c
Mikhail Lapin <greenisha@gmail.com>
2023-01-14 10:54:49 +0300

    some more tests

--- app/tests/unit/AveragePostsPerUserPerMonthTest.php
+++ app/tests/unit/AveragePostsPerUserPerMonthTest.php
@@ -21,33 +21,56 @@ use function PHPUnit\Framework\assertEquals;
  */
 class AveragePostsPerUserPerMonthTest extends TestCase
 {
+    /**
+     * Check if works
+     */
     public function testDefault(): void
     {
         $data = $this->prepareData('/srv/sm_assignment/tests/data/social-posts-response.json', new DateTime('2018-01-01'), new DateTime('2022-01-01'));
         $value = $data->getChildren()[0];
         assertEquals(1,$value->getValue());
     }
+    /**
+     * Check if dates in params is used to filter data
+     */
     public function testWrongDates(): void
     {
         $data = $this->prepareData('/srv/sm_assignment/tests/data/social-posts-response.json', new DateTime('2022-01-01'), new DateTime('2022-01-01'));
         assertEmpty($data->getChildren());
     }
+    /**
+     * Check same month in different years
+     */
     public function testDifferentYears():void
     {
         $data = $this->prepareData('/srv/sm_assignment/tests/data/social-posts-response-different-years.json', new DateTime('2018-01-01'), new DateTime('2022-01-01'));
         assertEquals(1, $data->getChildren()[0]->getValue());
         assertEquals(2, $data->getChildren()[1]->getValue());               
     }
+    /**
+     * Check precision and rounding
+     */
     public function testPrecision(): void
     {
         $data = $this->prepareData('/srv/sm_assignment/tests/data/social-posts-response-precision.json', new DateTime('2018-01-01'), new DateTime('2022-01-01'));
-        assertEquals(1, $data->getChildren()[0]->getValue());
-        assertEquals(1.5, $data->getChildren()[1]->getValue());
+        assertEquals(1.33, $data->getChildren()[0]->getValue());
     }
+    /**
+     * Empty data- empty statistics
+     */
     public function testNoData(): void
     {
         $data = $this->prepareData('/srv/sm_assignment/tests/data/social-posts-response-empty.json', new DateTime('2018-01-01'), new DateTime('2022-01-01'));
         assertEmpty($data->getChildren());
+    }
+    /**
+     * If some posts don't have user id - they won't count
+     */
+    public function testNoUserId(): void
+    {
+        $data = $this->prepareData('/srv/sm_assignment/tests/data/social-posts-response-no-user-id.json', new DateTime('2018-01-01'), new DateTime('2022-01-01'));
+        assertEquals(1, $data->getChildren()[0]->getValue());
+       
     }
     /**
      * Gets data from file and processes
